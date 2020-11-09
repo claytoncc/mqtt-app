98198fefa915a9a56b682f3780c542d421c35333
claytoncc <clayton@openiot.co.za>
2020-11-09 20:20:11 +0200

    updates

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-const mqtt = require('./mqtt');
-
+const MqttSocket = require('./mqtt');
+const mqtt = new MqttSocket();
 mqtt.connect({
     'host': 'mqtt://bitid.co.za',
     'port': 1888,
     'username': 'telemetry',
-    'password': 'telemetry1!',
-    'subscribe': {
-        'data': 'rock/v1.1/data',
-        'control': 'rock/v1.1/control'
-    }
+    'password': 'telemetry1!'
 });
--- mqtt.js
+++ mqtt.js
@@ -1,4 +1,4 @@
-const MQTT = require('mqtt');
+const mqtt = require('mqtt');
 const EventEmitter = require('events').EventEmitter;
 
 class MqttSocket extends EventEmitter {
@@ -8,71 +8,18 @@ class MqttSocket extends EventEmitter {
     };
 
     connect(server) {
-        try {
-            console.log('Connecting to mqtt: ' + JSON.stringify(server));
+        console.log('Connecting');
 
-            this.mqtt = MQTT.connect(server.host, {
-                'host': server.host,
-                'port': server.port,
-                'clean': true,
-                'username': server.username,
-                'password': server.password,
-                'keepalive': 60
-            });
+        const client = mqtt.connect('mqtt://bitid.co.za', {
+            'host': 'mqtt://bitid.co.za',
+            'port': 1888,
+            'username': 'telemetry',
+            'password': 'telemetry1!'
+        });
 
-            this.mqtt.on('end', () => {
-                console.log('Connecting to mqtt: END');
-            });
-
-            this.mqtt.on('close', () => {
-                console.log('Connecting to mqtt: CLOSE');
-            });
-
-            this.mqtt.on('offline', () => {
-                console.log('Connecting to mqtt: OFFLINE');
-            });
-
-            this.mqtt.on('disconnect', () => {
-                console.log('Connecting to mqtt: DISCONNECT');
-            });
-
-            this.mqtt.on('error', error => {
-                console.log(error);
-            });
-    
-            this.mqtt.on('connect', () => {
-                console.log('Connecting to mqtt: Success');
-
-                this.mqtt.subscribe(server.subscribe.data, (error) => {
-                    if (error) {
-                        console.log(error.message);
-                    } else {
-                        console.log('Subscribed to mqtt data');
-                    };
-                });
-    
-                this.mqtt.subscribe(server.subscribe.control, (error) => {
-                    if (error) {
-                        console.log(error.message);
-                    } else {
-                        console.log('Subscribed to mqtt control');
-                    };
-                });
-            });
-    
-            this.mqtt.on('message', (topic, message) => {
-                switch (topic) {
-                    case ('rock/v1.1/data'):
-                        this.emit('data', JSON.parse(message.toString()));
-                        break;
-                    case ('rock/v1.1/control'):
-                        this.emit('control', JSON.parse(message.toString()));
-                        break;
-                };
-            });
-        } catch (error) {
-            console.log(error.message);   
-        };
+        client.on('connect', () => {
+            console.log('Connected');
+        });
     };
 
     send(topic, payload) {
